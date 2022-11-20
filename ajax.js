e743480739ffebac5a3c5632e1ffe5e6e1a25f29
AgafiaQuantoriPythonSchool <agafia.temerova@quantori.academy>
2022-11-20 20:05:05 +0400

    sends rna and protein values from main.py to the form .html

--- ajax.js
+++ ajax.js
@@ -1,23 +1,33 @@
 let httpRequest;
 document
-  .getElementById("ajaxButton")
-  .addEventListener("click", makeRequest);
+    .getElementById("ajaxButton")
+    .addEventListener("click", makeRequest);
 
 function makeRequest() {
-  httpRequest = new XMLHttpRequest();
+    httpRequest = new XMLHttpRequest();
 
-  httpRequest.onreadystatechange = showImage;
-  httpRequest.open("POST", "http://127.0.0.1:8000");
-  httpRequest.send();
+    let all_inputs = document.getElementById("all_inputs");
+    // formData is like dictionary in python ._. help me
+    const formData = new FormData(all_inputs);
+
+    httpRequest.onreadystatechange = showImage;
+    httpRequest.open("POST", "http://127.0.0.1:8000");
+    httpRequest.send(formData);
 }
 
 function showImage() {
-  if (httpRequest.readyState === XMLHttpRequest.DONE) {
-    if (httpRequest.status === 200) {
-      alert(httpRequest.responseText);
-    } else {
-      console.log("There was a problem with the request.");
-      alert("There was a problem with the request.");
+    if (httpRequest.readyState === XMLHttpRequest.DONE) {
+        if (httpRequest.status === 200) {
+            let json = JSON.parse(httpRequest.responseText)
+
+            let rna_input = document.getElementById("rna_id")
+            rna_input.value = json.rna
+
+            let protein_input = document.getElementById("protein_id")
+            protein_input.value = json.protein
+        } else {
+            console.log("There was a problem with the request.");
+            alert("There was a problem with the request.");
+        }
     }
-  }
 }
