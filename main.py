e31c349ef8a8e58d7dd0763e81a6f4f7e7289c91
AgafiaQuantoriPythonSchool <agafia.temerova@quantori.academy>
2022-11-20 18:59:36 +0400

    add CORS requests and endpoint

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from data.genetic_codes import *
 from data.base_classes import *
 from utilities import *
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
+app = FastAPI()
 
 genetic_code = dict_reverse_from_list(GENETIC_CODE)
 print(genetic_code)
@@ -13,10 +16,19 @@ for dna in DNA:
     temp_rna = convert_dna_to_rna(dna)
     my_result.append(convert_rna_to_protein(temp_rna, genetic_code))
 
-#
-# print(my_result)
-# print(my_result == RESULT)
-# print(RESULT)
+
+# for allow CORS requests
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 
+@app.post("/")
+async def root():
+    return {"message": "Hello World"}
 
+# type to console "uvicorn main:app --reload"
