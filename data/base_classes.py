b49ae52de9433474d961d7828f26563e211b172e
AgafiaQuantoriPythonSchool <agafia.temerova@quantori.academy>
2022-11-20 21:13:16 +0400

    class names were changed

--- data/base_classes.py
+++ data/base_classes.py
@@ -1,43 +1,38 @@
-from sqlalchemy import create_engine
 from sqlalchemy import Column, Integer, String
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import declarative_base, relationship, backref
-from sqlalchemy.orm import sessionmaker
-
-# engine = create_engine("sqlite:///my_base.db")
-# session = sessionmaker(bind = engine)
 
 Base = declarative_base()
 
 
-class Dna_base(Base):
+class DnaBase(Base):
     __tablename__ = "dna_base"
     __table_args__ = {"extend_existing": True}
 
     id = Column(Integer, primary_key=True)
     name = Column(String(1))
-    rna = relationship("Rna_base", backref=backref("rna_base", uselist=False))
+    rna = relationship("RnaBase", backref=backref("rna_base", uselist=False))
     rna_id = Column(Integer, ForeignKey("rna_base.id"))
 
     def __str__(self):
         return f"{self.name}"
 
 
-class Rna_base(Base):
+class RnaBase(Base):
     __tablename__ = "rna_base"
     __table_args__ = {"extend_existing": True}
 
     id = Column(Integer, primary_key=True)
     name = Column(String(1))
 
-    # dna = relationship("Dna_base")
+    # dna = relationship("DnaBase")
     # dna_id = Column(Integer, ForeignKey("dna_base.id"))
 
     def __str__(self):
         return f"{self.name}"
 
 
-class Amino_acids(Base):
+class AminoAcids(Base):
     __tablename__ = "amino_acids"
     __table_args__ = {"extend_existing": True}
 
@@ -45,10 +40,6 @@ class Amino_acids(Base):
     short_name = Column(String(1))
     middle_name = Column(String(4))
     full_name = Column(String(30))
-
-    # codon = relationship("Codons")
-    # codon_id = Column(Integer, ForeignKey("codons.id"))
-    # amino_codons = []
 
     def __str__(self):
         return f"{self.short_name}: {self.middle_name}, ({self.full_name})"
@@ -61,8 +52,7 @@ class Codons(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String(3))
     amino_id = Column(Integer, ForeignKey("amino_acids.id"))
-    amino = relationship("Amino_acids", backref="amino_codons")
+    amino = relationship("AminoAcids", backref="amino_codons")
 
     def __str__(self):
         return f'{self.name}'
-
