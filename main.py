db7325c145e96cecc8e424cd5dcbe3978b151fc4
AgafiaQuantoriPythonSchool <agafia.temerova@quantori.academy>
2022-11-22 11:20:43 +0400

    now functions are working with db

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from data.genetic_codes import *
-from data.base_classes import *
-from utilities import convert_dna_to_rna, convert_rna_to_protein
+from data.genetic_codes import GENETIC_CODE_REVERSE
 from fastapi import FastAPI, Form
 from fastapi.middleware.cors import CORSMiddleware
+from data.base_classes import convert_dna_to_rna, convert_rna_to_protein
+from data.db import Session
 
 app = FastAPI()
 
@@ -18,8 +18,8 @@ app.add_middleware(
 
 @app.post("/")
 async def root(dna_string: str = Form()):
-    rna = convert_dna_to_rna(dna_string)
-    protein = convert_rna_to_protein(rna, GENETIC_CODE_REVERSE)
+    rna = convert_dna_to_rna(Session(), dna_string)
+    protein = convert_rna_to_protein(Session(), rna)
     return {"rna": rna, "protein": protein}
 
 # type to console "uvicorn main:app --reload"
