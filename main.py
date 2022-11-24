ea089dc435a96997d1460cab2981f27c1300d637
AgafiaQuantoriPythonSchool <agafia.temerova@quantori.academy>
2022-11-24 16:23:43 +0400

    + import create_db.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, Form
 from fastapi.middleware.cors import CORSMiddleware
 from data.base_classes import convert_dna_to_rna, convert_rna_to_protein
 from data.db import Session
+import data.create_db
 from utilities import plot_gc_ratio
 
 app = FastAPI()
@@ -17,11 +18,13 @@ app.add_middleware(
 
 # ATTTGGCTACTAACAATCTAGTTGTAATGGCCTACATTACAGGTGGTGTTGTTCAGTTGCTAACTAACGCTAACTAACATCTTTGGCACTGTTTATGAAAAACTCAAACCCGTCCTTGATTGGCTTGAAGAGAAGTTT
 
-@app.post("/")
+
+@app.post("/dna_to_rna/")
 async def root(dna_string: str = Form()):
     rna = convert_dna_to_rna(Session(), dna_string)
     protein = convert_rna_to_protein(Session(), rna)
     return {"rna": rna, "protein": protein}
+
 
 @app.post("/plot/")
 async def root(for_plot_string: str = Form(), step: int = Form()):
