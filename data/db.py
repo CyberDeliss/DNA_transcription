b04a2160cfd9d24e57c1204aa9e4ad03b7a7ff8d
AgafiaQuantoriPythonSchool <agafia.temerova@quantori.academy>
2022-11-24 16:22:05 +0400

    changed engine path

--- data/db.py
+++ data/db.py
@@ -2,8 +2,18 @@ from sqlalchemy.orm import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+# replace it to config:
+user = "test"
+password = "test"
+host = "localhost"
+port = "5432"
+db_name = "dna_transcription"
+
+SQLALCHEMY_DATABASE_URL = f"postgresql://{user}:{password}@{host}:{port}/{db_name}"
+
 Base = declarative_base()
-engine = create_engine("sqlite:///data/my_base.db")
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+# engine = create_engine("sqlite:///data/my_base.db")
 Session = sessionmaker(bind=engine)
 
 
