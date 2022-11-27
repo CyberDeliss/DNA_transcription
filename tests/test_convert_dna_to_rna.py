90a5d23bb9055e0dd584fa4510ea31eb9ace3787
AgafiaQuantoriPythonSchool <agafia.temerova@quantori.academy>
2022-11-27 19:43:15 +0400

    + test_basic

--- tests/test_convert_dna_to_rna.py
+++ tests/test_convert_dna_to_rna.py
@@ -1,52 +1,57 @@
 import unittest
+from unittest.mock import patch, Mock
 from data.base_classes import convert_dna_to_rna
-from data.db import get_db
+
+DNA_LETTER_CONVERT = {
+    "A": "A",
+    "C": "C",
+    "G": "G",
+    "T": "U"
+}
+WRONG_MESSAGE = "Your DNA string is wrong"
+
+# mock = Mock()
+# mock.method.return_value = [3, 2]
+# print(mock.method())
 
 
 class TestConvertDNAtoRNA(unittest.TestCase):
-
     def setUp(self) -> None:
-        db = get_db()
-        dna_string = ""
-        expexted = ""
-        actual = convert_dna_to_rna(db, dna_string)
-
-        self.assertTrue(actual == expexted, f"Should be {expexted}")
+        self.db = ""
 
     def test_basic(self):
-        db = get_db()
-        dna_string = ""
-        expexted = ""
-        actual = convert_dna_to_rna(db, dna_string)
+        dna_string = "CCCGTCCTTGATTGGCTTGAAGAGAAGTTT"
+        expected = "CCCGUCCUUGAUUGGCUUGAAGAGAAGUUU"
 
-        self.assertTrue(actual == expexted, f"Should be {expexted}")
+        with patch('data.base_classes.convert_dna_letter_to_rna_letter') as mocked_rna_letter:
+            mocked_rna_letter.side_effect = [DNA_LETTER_CONVERT[dna] for dna in dna_string]
+            actual = convert_dna_to_rna(self.db, dna_string)
+
+        print([DNA_LETTER_CONVERT[dna] for dna in dna_string])
+        self.assertTrue(actual == expected, f"{actual}. But should be {expected}")
 
     def test_dna_no_str(self):
-        db = get_db()
-        dna_string = ""
-        expexted = ""
-        actual = convert_dna_to_rna(db, dna_string)
-
-        self.assertTrue(actual == expexted, f"Should be {expexted}")
-
-    def test_db_no_exist(self):
-        db = get_db()
-        dna_string = ""
-        expexted = ""
+        dna_string = "123456789"
+        expexted = WRONG_MESSAGE
         actual = convert_dna_to_rna(db, dna_string)
 
         self.assertTrue(actual == expexted, f"Should be {expexted}")
 
     def test_dna_lower(self):
-        db = get_db()
         dna_string = ""
         expexted = ""
         actual = convert_dna_to_rna(db, dna_string)
 
         self.assertTrue(actual == expexted, f"Should be {expexted}")
 
-    def test_letters_is_wrong(self):
-        db = get_db()
+    def test_dna_register_is_various(self):
+        dna_string = ""
+        expexted = ""
+        actual = convert_dna_to_rna(db, dna_string)
+
+        self.assertTrue(actual == expexted, f"Should be {expexted}")
+
+    def test_contains_invalid_letters(self):
         dna_string = ""
         expexted = ""
         actual = convert_dna_to_rna(db, dna_string)
@@ -54,7 +59,6 @@ class TestConvertDNAtoRNA(unittest.TestCase):
         self.assertTrue(actual == expexted, f"Should be {expexted}")
 
     def test_a_lot_of_letters(self):
-        db = get_db()
         dna_string = ""
         expexted = ""
         actual = convert_dna_to_rna(db, dna_string)
@@ -62,7 +66,6 @@ class TestConvertDNAtoRNA(unittest.TestCase):
         self.assertTrue(actual == expexted, f"Should be {expexted}")
 
     def test_too_few_letters(self):
-        db = get_db()
         dna_string = ""
         expexted = ""
         actual = convert_dna_to_rna(db, dna_string)
