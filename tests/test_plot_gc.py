045555359be845432abab6ba0422dc27ee0b6f29
AgafiaQuantoriPythonSchool <agafia.temerova@quantori.academy>
2022-11-28 05:24:43 +0400

    + tests for dna string and var step

--- tests/test_plot_gc.py
+++ tests/test_plot_gc.py
@@ -3,7 +3,7 @@ from utilities import plot_gc_ratio
 from unittest.mock import patch
 
 
-WRONG_MESSAGE = ""
+WRONG_MESSAGE = "Your DNA string is wrong"
 EMPTY_MESSAGE = ""
 
 
@@ -12,30 +12,94 @@ class TestPlotGC(unittest.TestCase):
         pass
 
     def test_basic(self):
-        dna_string = "CCCGTCCTTGATTGGCTTGAAGAGAAGTTT"
+        dna_string = "ATTTGGCTACTAACAATCTA"
+        expected = True
+
+        with patch('utilities.create_img') as mocked_create_img:
+            mocked_create_img.return_value = True
+            actual = plot_gc_ratio(dna_string)
+
+        self.assertTrue(actual, f"{actual}. But should be {expected}")
+
+    def test_dna_no_str(self):
+        dna_string = 123
         step = 10
-        expected = None
+        expected = False
+        with patch('utilities.create_img') as mocked_create_img:
+            mocked_create_img.return_value = True
+            actual = plot_gc_ratio(dna_string, step)
+
+        self.assertFalse(actual, f"{actual}. But should be {expected}")
+
+    def test_step_isnt_int(self):
+        dna_string = "CCCGTCCTTGATTGGCTTGAAGAGAAGTTT"
+        step = 10.12345
+        expected = False
 
         with patch('utilities.create_img') as mocked_create_img:
             mocked_create_img.return_value = True
             actual = plot_gc_ratio(dna_string, step)
 
-        self.assertIsNone(actual, f"{actual}. But should be {expected}")
-
-    def test_no_str(self):
-        pass
-
-    def test_step_is_int(self):
-        pass
+        self.assertFalse(actual, f"{actual}. But should be {expected}")
 
     def test_step_lower_0(self):
-        pass
+        dna_string = "CCCGTCCTTGATTGGCTTGAAGAGAAGTTT"
+        step = -1
+        expected = False
+
+        with patch('utilities.create_img') as mocked_create_img:
+            mocked_create_img.return_value = True
+            actual = plot_gc_ratio(dna_string, step)
+
+        self.assertFalse(actual, f"{actual}. But should be {expected}")
+
+    def test_step_lower_0_and_is_float(self):
+        dna_string = "CCCGTCCTTGATTGGCTTGAAGAGAAGTTT"
+        step = -1.345
+        expected = False
+
+        with patch('utilities.create_img') as mocked_create_img:
+            mocked_create_img.return_value = True
+            actual = plot_gc_ratio(dna_string, step)
+
+        self.assertFalse(actual, f"{actual}. But should be {expected}")
 
     def test_img_create(self):
+        # TODO add os
         pass
 
-    def test_step_is_float(self):
-        pass
+    def test_step_is_string(self):
+        dna_string = "CCCGTCCTTGATTGGCTTGAAGAGAAGTTT"
+        step = "10"
+        expected = False
+
+        with patch('utilities.create_img') as mocked_create_img:
+            mocked_create_img.return_value = True
+            actual = plot_gc_ratio(dna_string, step)
+
+        self.assertFalse(actual, f"{actual}. But should be {expected}")
+
+    def test_dna_lower(self):
+        dna_string = "CCCGTCCTTGATTGGCTTGAAGAGAAGTTT".lower()
+        step = 10
+        expected = True
+
+        with patch('utilities.create_img') as mocked_create_img:
+            mocked_create_img.return_value = True
+            actual = plot_gc_ratio(dna_string, step)
+
+        self.assertFalse(actual, f"{actual}. But should be {expected}")
+
+    def test_dna_wrong_alphabet(self):
+        dna_string = "sdfg5678_=865 5+* # <<>/.\|| <> hhn".lower()
+        step = 10
+        expected = False
+
+        with patch('utilities.create_img') as mocked_create_img:
+            mocked_create_img.return_value = True
+            actual = plot_gc_ratio(dna_string, step)
+
+        self.assertFalse(actual, f"{actual}. But should be {expected}")
 
 
 if __name__ == '__main__':
