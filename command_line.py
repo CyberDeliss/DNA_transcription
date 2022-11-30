223224b53cba4afe097c76bd6089647baeff5bce
AgafiaQuantoriPythonSchool <agafia.temerova@quantori.academy>
2022-11-30 21:52:35 +0400

    added args DnaToRna, RnaToProtein, PlotGcRatio, Step

--- command_line.py
+++ command_line.py
@@ -1,8 +1,8 @@
-import getopt
-import sys
 import argparse
-# from utilities import plot_gc_ratio
-# from data.base_classes import convert_dna_to_rna, convert_rna_to_protein
+from data.db import Session
+from utilities import plot_gc_ratio
+from data.base_classes import convert_dna_to_rna, convert_rna_to_protein
+
 
 # Initialize parser
 parser = argparse.ArgumentParser()
@@ -11,16 +11,29 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-dna", "--DnaToRna", help="convert DNA to RNA")
 parser.add_argument("-rna", "--RnaToProtein", help="Convert RNA to protein")
 parser.add_argument("-p", "--PlotGcRatio", help="open image")
+parser.add_argument("-s", "--Step", help="denoting a width of a bin")
 
 # Read arguments from command line
 args = parser.parse_args()
 
 if args.DnaToRna:
-    print("Displaying DnaToRna as: % s" % args.DnaToRna)
+    print(f"Displaying DnaToRna as: {args.DnaToRna}")
+    print(convert_dna_to_rna(Session(), args.DnaToRna))
 
 if args.RnaToProtein:
-    print("Displaying RnaToProtein as: % s" % args.RnaToProtein)
+    print(f"Displaying RnaToProtein as: {args.RnaToProtein}")
+    print(convert_rna_to_protein(Session(), args.RnaToProtein))
 
-if args.PlotGcRatio:
-    print("Displaying PlotGcRatio as: % s" % args.PlotGcRatio)
+if args.PlotGcRatio and args.Step:
+    print(f"Displaying PlotGcRatio as: {args.PlotGcRatio}, step: {args.Step}")
+    if plot_gc_ratio(args.PlotGcRatio, int(args.Step)):
+        print("True")
+    else:
+        print("False")
 
+elif args.PlotGcRatio:
+    print(f"Displaying PlotGcRatio as: {args.PlotGcRatio}")
+    if plot_gc_ratio(args.PlotGcRatio):
+        print("True")
+    else:
+        print("False")
