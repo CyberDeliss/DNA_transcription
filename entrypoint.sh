ccd12f279ad0330255718021fd14bcf3bdb4241f
AgafiaQuantoriPythonSchool <agafia.temerova@quantori.academy>
2022-11-30 17:22:45 +0400

    changed condition for sleep

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,11 @@
 
 echo "Waiting for postgres..."
 
-#while ! nc -z 127.0.0.1 5432; do
-#  sleep 0.1
-#done
+while `nc -z 127.0.0.1 5432`; do
+  sleep 0.1
+done
 
-sleep 10
+#sleep 10
 
 
 echo "PostgreSQL started"
