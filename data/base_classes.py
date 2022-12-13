3a74106211065ac0909ac0127543f7beff1eccc5
CyberDeliss <ganyajob@gmail.com>
2022-12-13 13:50:39 +0400

    eased the conditions

--- data/base_classes.py
+++ data/base_classes.py
@@ -62,7 +62,8 @@ def convert_dna_letter_to_rna_letter(db: Session, dna_letter: str) -> RnaBase:
 
     :return: rna as RnaBase
     """
-    return db.execute(select(RnaBase).join_from(DnaBase, RnaBase).filter(DnaBase.name == dna_letter)).scalar()
+    rna_base = db.execute(select(RnaBase).join_from(DnaBase, RnaBase).filter(DnaBase.name == dna_letter)).scalar()
+    return rna_base
 
 
 def convert_codon_to_amino(db: Session, codon: str) -> AminoAcids:
@@ -73,19 +74,22 @@ def convert_codon_to_amino(db: Session, codon: str) -> AminoAcids:
     :return: amino as AminoAcids object
     """
     if len(codon) == 3:
-        return db.execute(select(AminoAcids).join_from(Codons, AminoAcids).filter(Codons.name == codon)).scalar()
+        amino = db.execute(select(AminoAcids).join_from(Codons, AminoAcids).filter(Codons.name == codon)).scalar()
+        return amino
 
 
 def convert_dna_to_rna(db: Session, dna_string: str) -> str:
+    if not dna_string:
+        return "Your DNA string is empty"
+
     result_str = ""
     dna_string = dna_string.upper()
-    if dna_string:
-        if check_dna_string(dna_string):
-            for dna in dna_string:
-                result_str += convert_dna_letter_to_rna_letter(db, dna).name
-            return result_str
-        return "Your DNA string is wrong"
-    return "Your DNA string is empty"
+
+    if check_dna_string(dna_string):
+        for dna in dna_string:
+            result_str += convert_dna_letter_to_rna_letter(db, dna).name
+        return result_str
+    return "Your DNA string is wrong"
 
 
 def get_codons(rna_string: str) -> list:
@@ -99,17 +103,19 @@ def convert_rna_to_protein(db: Session, rna_string: str) -> str:
     :param rna_string: RNA as str
     :return: protein string
     """
+    if not rna_string:
+        return "Your RNA string is empty"
+
     result_string = ""
     rna_string = rna_string.upper()
-    if rna_string:
-        if check_rna_string(rna_string):
-            codons = get_codons(rna_string)
-            for codon in codons:
-                if len(codon) == 3:
-                    result_string += convert_codon_to_amino(db, codon).short_name
-            return result_string
-        return f"Your RNA string is wrong"
-    return "Your RNA string is empty"
+
+    if check_rna_string(rna_string):
+        codons = get_codons(rna_string)
+        for codon in codons:
+            if len(codon) == 3:
+                result_string += convert_codon_to_amino(db, codon).short_name
+        return result_string
+    return f"Your RNA string is wrong"
 
 
 def check_dna_string(dna_string: str) -> bool:
